a78a85a7c2364e538ad6bf79fb165461e009e754
swathi-ops <swathipottabathini@gmail.com>
2021-09-07 05:38:06 +0530

    first

--- firebase_crud/src/firebase.js
+++ firebase_crud/src/firebase.js
@@ -1,15 +1,13 @@
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyA-MB617jubZ6UEA88rLdyWYbG6cGOZuVw",
-  authDomain: "react-crud-4ccd1.firebaseapp.com",
-  databaseURL: "https://react-crud-4ccd1-default-rtdb.firebaseio.com",
-  projectId: "react-crud-4ccd1",
-  storageBucket: "react-crud-4ccd1.appspot.com",
-  messagingSenderId: "962026649466",
-  appId: "1:962026649466:web:2244eebbbd6675041a48c3"
-  };
-  // Initialize Firebase
-  var fireDb = firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCrXntygrVcEbyeWdJThmkmF3KuiipIk8s",
+  authDomain: "test-d5a67.firebaseapp.com",
+  projectId: "test-d5a67",
+  storageBucket: "test-d5a67.appspot.com",
+  messagingSenderId: "276956573667",
+  appId: "1:276956573667:web:0d31b5031359422774bad6"
+};
 
-  export default fireDb.database().ref();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
